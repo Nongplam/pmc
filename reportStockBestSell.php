0a00b2f4c31856162e152581c7f033cbe9e44207
MRoSlot <pop.1413@gmail.com>
2018-03-27 11:14:44 +0700

    ใช้ role desc ใน rolesetting

--- reportStockBestSell.php
+++ reportStockBestSell.php
@@ -6,25 +6,33 @@
     <script src="js/lib/jquery-3.3.1.min.js"></script>
     <script src="js/lib/bootstrap.min.js" type="text/javascript"></script>
 
-
-
 </head>
 
 <body>
-
+    <?php 
+    include 'mainbartest.php';
+    $role=$_SESSION["role"];
+    $allowquery="SELECT rule FROM `rolesetting` WHERE rolesetting.rolename = '$role'";
+    $allowqueryresult=mysqli_query($con,$allowquery);
+    $allowruleraw=$allowqueryresult->fetch_array(MYSQLI_ASSOC);    
+    $allowrule = explode(",",$allowruleraw["rule"]);
+        if (!in_array("8", $allowrule)){
+            header("Location: auth.php");
+        }
+     ?>
     <div class="container" style="width:70%">
         <h3 align="center">รายงานสินค้าขายดี แต่ละสาขา</h3>
         <div ng-app="reportStockBestSellApp" ng-controller="reportStockBestSellcontroller" class="ng-scope">
-<form>
+            <form>
 
-สาขา :
- <select  id="branch" name="branch" ng-model="branch" class="form-control custom-select" ng-init="selectBranch()" >
+                สาขา :
+                <select id="branch" name="branch" ng-model="branch" class="form-control custom-select" ng-init="selectBranch()">
 <option ng-repeat="branch in branchs" value="{{branch.id}}">{{branch.name}}</option>
     
 </select>
-<input type="submit" name="" ng-click="getAllStock()" class="btn btn-success" value="ตกลง" style="width: 117px;">
-</form>
-     <table class="table table-info table-bordered" >
+                <input type="submit" name="" ng-click="getAllStock()" class="btn btn-success" value="ตกลง" style="width: 117px;">
+            </form>
+            <table class="table table-info table-bordered">
                 <tbody>
                     <tr>
                         <th>เลขสต็อค</th>
@@ -35,22 +43,22 @@
                         <th>ทั้งหมด</th>
                         <th>หน่วย</th>
                         <th>วันที่รับ</th>
-                      
+
                     </tr>
-                    <tr  ng-repeat= "stock in stocks" >
+                    <tr ng-repeat="stock in stocks">
                         <td>{{stock.sid}}</td>
                         <td>{{stock.pname}}</td>
                         <td>{{stock.productid}}</td>
                         <td>{{stock.bname}}</td>
                         <td>{{stock.qtyall}}</td>
                         <td>{{stock.remainall}}</td>
-                        <td>{{stock.stocktype}}</td>                       
+                        <td>{{stock.stocktype}}</td>
                         <td>{{stock.receiveday}}</td>
-                        
 
 
 
-                       
+
+
 
                     </tr>
 
@@ -58,13 +66,12 @@
             </table>
 
 
-  </div>
+        </div>
     </div>
 
 
 
 
-   <script src="js/reportStockBestSell.js"></script>
+    <script src="js/reportStockBestSell.js"></script>
     <script src="dist/sweetalert.min.js"></script>
 </body>
- 
