5167a08ead96930fc53e789ace2fbc126d06f565
MRoSlot <pop.1413@gmail.com>
2018-04-18 19:11:58 +0700

    test

---
+++ php/testapi.php
@@ -0,0 +1,18 @@
+<?php
+/*include 'connectDB.php';
+$stm1="SELECT * FROM user";
+$result=mysqli_query($con,$stm1);
+$res = array();
+while ($rows = $result->fetch_array(MYSQLI_ASSOC)) {
+	# code...
+	$res[] = array('userid'=>$rows['id'],'fname'=> $rows['fname'],'lname'=>$rows['lname']);
+
+}
+$user['records'] = $res;
+echo json_encode($user);*/
+
+?>
+<?php
+	include 'connectDB.php';
+	$stm1="SELECT * from"
+?>
