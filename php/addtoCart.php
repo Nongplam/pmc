84550c6e9b0399c90b577f92c4b66dba57242fd1
MRoSlot <pop.1413@gmail.com>
2018-05-26 19:37:31 +0700

    kaiupdate barcode and autofocus

--- php/addtoCart.php
+++ php/addtoCart.php
@@ -10,17 +10,37 @@ $subbranchid = $_SESSION["subbranchid"];
     
 if($data){
     $stockid=mysqli_real_escape_string($con, $data->stockid);
-$price=mysqli_real_escape_string($con, $data->price);
-$qty=mysqli_real_escape_string($con, $data->qty);
+    $price=mysqli_real_escape_string($con, $data->price);
+    $qty=mysqli_real_escape_string($con, $data->qty);
     
+    $searchdupQuery="SELECT * FROM `pos` WHERE `stockid` = '$stockid' AND `subbranchid` = '$subbranchid'";
+    $dupresult = mysqli_query($con,$searchdupQuery);
     
-    $query="insert into pos(stockid,price,qty,userid,subbranchid) values('$stockid','$price','$qty','$userid','$subbranchid')";
+    $dup=array();
+    $oldqtyarr=array();
+    while ($rows = $dupresult->fetch_array(MYSQLI_ASSOC)){
+    array_push($dup,$rows['id']);
+    array_push($oldqtyarr,$rows['qty']);
+    }
+        
+    if(sizeof($dup) != 0){
+        $posid = $dup[0];
+        $oldqty = $oldqtyarr[0];
+        $newqty = $qty + $oldqty;
+        $query="UPDATE `pos` SET `qty` = '$newqty' WHERE `pos`.`id` = '$posid'";
+        if(mysqli_query($con, $query)) {
+            echo "Data Updated";
+        }
+        else {
+            echo "Error";
+        }
+    }else{
+        $query="insert into pos(stockid,price,qty,userid,subbranchid) values('$stockid','$price','$qty','$userid','$subbranchid')";
         if(mysqli_query($con, $query)) {
             echo "Data Inserted";
         }
         else {
             echo "Error";
         }
+    }  
 }
-
-
